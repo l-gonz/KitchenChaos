501c6aa837d2b0dcb142ecf32cc08d308cbd5570
Laura Gonzalez <l.gonzalezfernan@gmail.com>
2025-03-19 19:20:41 +0100

    Add movement collisions

--- Assets/Project/Player.cs
+++ Assets/Project/Player.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private const float PLAYER_RADIUS = .7f;
+    private const float PLAYER_HEIGHT = 2f;
+
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 5f;
 
@@ -13,9 +16,27 @@ public class Player : MonoBehaviour
     {
         var inputVector = gameInput.GetMoveInputVectorNormalized();
         var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        var moveDistance = moveSpeed * Time.deltaTime;
+
         IsWalking = moveDirection.magnitude > 0;
 
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
-        transform.forward = Vector3.Slerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
+        if (!IsWalking) return;
+        
+        var isObstacleForward = Physics.CapsuleCast(transform.position,
+            transform.position + Vector3.up * PLAYER_HEIGHT, PLAYER_RADIUS, 
+            Vector3.Scale(moveDirection, Vector3.forward), moveDistance);
+        var isObstacleRight = Physics.CapsuleCast(transform.position,
+            transform.position + Vector3.up * PLAYER_HEIGHT, PLAYER_RADIUS, 
+            Vector3.Scale(moveDirection, Vector3.right), moveDistance);
+
+        if (isObstacleForward && isObstacleRight) return;
+
+        var collidedMoveDirection = new Vector3(!isObstacleRight ? moveDirection.x : 0, 0,
+            !isObstacleForward ? moveDirection.z : 0);
+        collidedMoveDirection.Normalize();
+
+        transform.position += collidedMoveDirection * moveDistance;
+        transform.forward = Vector3.Slerp(transform.forward, collidedMoveDirection,
+            rotateSpeed * Time.deltaTime);
     }
 }
